b186bca39f6dfc221b846bd96127e14d0e488c44
JanetHardiman <dinmahr@gmail.com>
2017-01-24 12:08:00 +0000

    New Repository

---
+++ src/com/company/FaceDetector.java
@@ -0,0 +1,87 @@
+package com.company;
+
+import org.opencv.core.*;
+import org.opencv.core.Point;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Dinmahr on 29/10/2016.
+ */
+class FaceDetector {
+    private CascadeClassifier face_cascade, eye_cascade;
+    // Create a constructor method
+    public FaceDetector(){
+
+        face_cascade = new CascadeClassifier("haarcascade_frontalface_alt.xml");
+        eye_cascade = new CascadeClassifier("haarcascade_eye.xml");
+        if(face_cascade.empty())
+        {
+            System.out.println("--(!)Error loading A\n");
+            return;
+        }
+        else
+        {
+            System.out.println("Face classifier loooaaaaaded up");
+        }
+    }
+    public Mat detect(Mat inputframe){
+        Mat mRgba=new Mat();
+        Mat mGrey=new Mat();
+        MatOfRect faces = new MatOfRect();
+        inputframe.copyTo(mRgba);
+        inputframe.copyTo(mGrey);
+        Imgproc.cvtColor( mRgba, mGrey, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.equalizeHist( mGrey, mGrey );
+        face_cascade.detectMultiScale(mGrey, faces);
+        System.out.println(String.format("Detected %s faces", faces.toArray().length));
+        Rect rectCrop=null;
+        for(Rect rect:faces.toArray())
+        {
+//            Point center= new Point(rect.x + rect.width*0.5, rect.y + rect.height*0.5 );
+            //draw a blue eclipse around face - didn't work for me
+//            Size s = new Size( rect.width*0.5, rect.height*0.5), 0, 0, 360, new Scalar( 0, 0, 255 );
+//            Core.ellipse( mRgba, center,s , 4, 8, 0 );
+            //draw a pink eclipse around face
+//            Core.ellipse( mRgba, center, new Size( rect.width*0.5, rect.height*0.5), 0, 0, 360, new Scalar( 255, 0, 255 ), 4, 8, 0 );
+
+
+            //draw a bounding box around each face
+            Core.rectangle(mRgba, //where to draw the box
+                    new Point(rect.x, rect.y), //bottom left
+                    new Point(rect.x + rect.width, rect.y + rect.height), //top right
+                    new Scalar(255,0,0));   //RGB colour - red
+            rectCrop = new Rect(rect.x, rect.y, rect.width, rect.height);
+        }
+//        Mat image_roi = new Mat(mRgba,rectCrop);
+//        Highgui.imwrite("cropimage_912.jpg",image_roi);
+
+/*        // Find the eyes and turn square in the array
+        MatOfRect eyes = new MatOfRect();
+        eye_cascade.detectMultiScale(mGrey, eyes);
+        for (Rect rect : eyes.toArray()) {
+            //Write text to upper left corner
+            Core.putText(mRgba, "Eye", new Point(rect.x,rect.y-5), 1, 2, new Scalar(255,0,0));
+            //Draw a square
+            Core.rectangle(mRgba, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
+                    new Scalar(200, 200, 100),2);
+            Point center= new Point(rect.x + rect.width*0.5, rect.y + rect.height*0.5 );
+            //draw a blue eclipse around face - didn't work for me
+            Size s = new Size( rect.width*0.5, rect.height*0.5);
+        //    Core.ellipse( mRgba, center,s , 4, 8, 0 );
+            //draw a pink eclipse around face
+            Core.ellipse( mRgba, center, new Size( rect.width*0.5, rect.height*0.5), 0, 0, 360, new Scalar( 255, 0, 255 ), 4, 8, 0 );
+        }*/
+
+        if(faces.toArray().length > 1){
+
+            System.out.println("More than 1 face detected, please ensure security!");
+        }
+
+        return mRgba;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,225 @@
+package com.company;
+/*Janet Hardiman G00305023
+ * 5th Year Project - Facial Recognition Part
+ * 07/10/2016 - Starting with Paul Dunne's code to Detect a Face from a still image and draw a rectangle box around it,
+ *              this code is using open source code from OPENCV (DetectFaceDemo)
+ * 14/10/2016 - Then used code to Capture one frame from a webcam (FrameCapture)
+ * 21/10/2016 - Next tried to detect a face in the WebCam frame capture and draw a box around it (WebCamFaceDetect)
+ *              the above three classes were called from the first main, one at a time - this is now commented out
+ * 28/10/2016 - Next stage live stream webcam image, detect face and draw a circle / rectangle around it.
+ *              this is ran from the second main method and uses (FacePanel - to open a JPanel and FaceDetector - as name suggests
+ *              & to draw circle / rectangle around the face.
+ * 04/11/2016 - research Facial Recognition! and update GitHub
+ * 11/11/2016 - spent the week researching eye detection to verify the person is real and not just shown an image
+ * 18/11/2016 - detect eyes and draw rectangles around them.
+ * 25/11/2016 - working on showing a live image and capturing a photo of it.
+ * 09/12/2016 - setting up raspberry pi for database
+ * 16/12/2016 - project demo
+ */
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.optional.ssh.Scp;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+ /*   public static void main(String[] args) throws Exception {
+        System.out.println("harr Face Detection with OpenCV");
+        // Load the native library.
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        new DetectFaceDemo().run();
+        try {
+        //    new FrameCapture().run();
+        //    new WebCamFaceDetect().run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }*/
+
+    public static void main(String arg[]) throws InterruptedException{
+        // Load the native library.
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        //or ...     System.loadLibrary("opencv_java244");
+
+        final VideoCapture[] webCam = {new VideoCapture(0)};
+
+        //make the JFrame
+        JFrame frame = new JFrame("WebCam Capture - Face detection");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FaceDetector faceDetector = new FaceDetector();
+        FacePanel facePanel = new FacePanel();
+        frame.setSize(400,400); //give the frame some arbitrary size
+        frame.setBackground(Color.BLUE);
+        frame.add(facePanel, BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+        JButton cancel = new JButton("Cancel");
+        JButton capture = new JButton("Capture");
+        JButton retake = new JButton("Retake");
+        JButton save = new JButton("Save");
+
+        buttonPanel.add(cancel);
+        cancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                webCam[0].release();
+                frame.dispose();
+            }
+        });
+
+        buttonPanel.add(capture);
+        capture.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Save the visualized detection.
+                try {
+                    new FrameCapture().run();
+                    new DetectFaceDemo().run();
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        /*buttonPanel.add(retake);
+        retake.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //open live webcam again
+                facePanel.repaint();
+                frame.repaint();
+                JOptionPane j1 = new JOptionPane(facePanel);
+                FacePanel f1 = new FacePanel();
+                j1.add(f1);
+                j1.setVisible(true);
+
+                webCam[0] = new VideoCapture(0);
+
+                try {
+                    System.out.println("i am here in try");
+                    VideoStream(webCam[0], frame, faceDetector, f1);
+                    System.out.println("try after function");
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                    System.out.println("i am here");
+                }
+
+                *//*new FaceDetector();
+                FacePanel face2Panel = new FacePanel();
+                frame.add(face2Panel, BorderLayout.CENTER);*//*
+            }
+        });*/
+
+
+        buttonPanel.add(save);
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                BufferedImage img = null;
+                try {
+                    img=ImageIO.read(new File("webCamImg.png"));
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                File theDir = new File("images");
+                if (!theDir.exists()) {
+                    System.out.println("creating directory: " + theDir);
+                    boolean result = false;
+
+                    try{
+                        theDir.mkdir();
+                        result = true;
+                    }
+                    catch(SecurityException se){
+                        //handle it
+                    }
+                    if(result) {
+                        System.out.println("DIR created");
+                    }
+                }
+                String path = "";
+                path = theDir.getAbsolutePath();
+                JOptionPane.showMessageDialog(facePanel, "saved to:  " + path);
+                System.out.println(path);
+
+
+                File f2 = new File(path + "\\myName.jpg");
+                String myName = "myName.jpg";
+
+                //File f2 = new File(path);  //output file path
+                try {
+                    ImageIO.write(img, "jpg", f2 );
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                sendToPi(myName);
+            }
+
+            private void sendToPi(String photo) {
+                // This make scp copy of
+                // one local file to remote dir
+                org.apache.tools.ant.taskdefs.optional.ssh.Scp scp = new Scp();
+                int portSSH = 22;
+                String srvrSSH = "192.168.43.184";
+                String userSSH = "pi";
+                String pswdSSH = "raspberry";
+                String localFile = "images/" + photo;
+                String remoteDir = "/home/pi/Desktop";
+
+                scp.setPort( portSSH );
+                scp.setLocalFile( localFile );
+                scp.setTodir( userSSH + ":" + pswdSSH + "@" + srvrSSH + ":" + remoteDir );
+                scp.setProject( new Project() );
+                scp.setTrust( true );
+                scp.execute();
+            }
+        });
+
+        frame.add(buttonPanel, BorderLayout.SOUTH);
+        frame.setVisible(true);
+
+        VideoStream(webCam[0], frame, faceDetector, facePanel);
+
+    } //end main
+
+
+    public static void VideoStream(VideoCapture webCam, JFrame frame, FaceDetector faceDetector, FacePanel facePanel) throws InterruptedException {
+        //Open and Read from the video stream
+        Mat webcam_image = new Mat();
+            //   VideoCapture webCam =new VideoCapture(0);
+
+        if (webCam.isOpened()) {
+            Thread.sleep(500); /// This one-time delay allows the Webcam to initialize itself
+            while (true) {
+                webCam.read(webcam_image);
+                if (!webcam_image.empty()) {
+                    Thread.sleep(200); /// This delay eases the computational load .. with little performance leakage
+                    frame.setSize(webcam_image.width() + 40, webcam_image.height() + 60);
+                    //Apply the classifier to the captured image
+                    webcam_image = faceDetector.detect(webcam_image);
+                    //Display the image
+                    facePanel.matToBufferedImage(webcam_image);
+                    facePanel.repaint();
+                    frame.setVisible(true);
+                } else {
+                    System.out.println(" --(!) live webcam closed ! --");
+                    break;
+                }
+            }
+        }
+        webCam.release(); //release the webcam
+        }
+
+}
